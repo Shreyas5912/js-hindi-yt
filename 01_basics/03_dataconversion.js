bcfab2bb67cdd2208cb896b6b930cfcd6bed89b4
Shreyas5912 <156225681+Shreyas5912@users.noreply.github.com>
2024-12-12 07:09:11 +0000

    DataType Conversion

---
+++ 01_basics/03_dataconversion.js
@@ -0,0 +1,26 @@
+/* let score = undefined;
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber); */
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+/* let isLoggedIn = 0;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn); */
+
+// 1 => True;
+// "" => False;
+// "Shreyas" => True;
+
+let someNumber = 33;
+let stringNummber = String(someNumber);
+console.log(stringNummber);
+console.log(typeof stringNummber);
