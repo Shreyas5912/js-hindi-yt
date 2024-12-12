52d3a7cb56b1d0b25c53fdba0567afc170661463
Shreyas5912 <156225681+Shreyas5912@users.noreply.github.com>
2024-12-12 06:54:40 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553;
+let accountEmail = "shreyas@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+
+//accountId = 2; Not Allowed
+
+accountEmail = "hshs@gmail.com"
+accountPassword = "1234"
+accountCity = "Nagpur"
+
+console.log(accountId);
+
+/* Do Not Used Var */
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //Treats all the js code as newer versions of js
+
+// We use alert in node js only not in browser
+
+/* console.log(3+3);
+
+console.log("Shreyas");
+
+write code in this format not to add new lines simulatneously 
+
+*/
+
+let name ="Shreyas";
+let age = 20;
+let isLoggedIn = false;
+
+
+//number => 2 to the pwr 53
+//bigint
+//string => ""
+//boolean => True/False
+//null => Standalnoe value
+//undefined => 
+//symbol => unique
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
